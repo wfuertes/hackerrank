afdf023850c2aa388fd19bf8a63902a0a8b58a93
Willian Batista <willian.batista@skipthedishes.ca>
2021-03-05 16:58:54 -0600

    ThrottlingGateway O(n)

--- src/main/java/interview/preparation/kit/challenges/ThrottlingGateway.java
+++ src/main/java/interview/preparation/kit/challenges/ThrottlingGateway.java
@@ -3,7 +3,7 @@ package interview.preparation.kit.challenges;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 /**
  * https://algo.monster/problems/throttling_gateway
@@ -11,47 +11,50 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ThrottlingGateway {
 
     public static int droppedRequests(List<Integer> requestTimes) {
-        final Map<Integer, Integer> timesAndRequests = new TreeMap<>();
-        final AtomicInteger dropped = new AtomicInteger(0);
+        final Map<Integer, Integer> requestNumbers = new TreeMap<>();
 
-        for (int i = 0; i < requestTimes.size(); i++) {
-            int requestTime = requestTimes.get(i);
-            timesAndRequests.putIfAbsent(requestTime, 0);
-            timesAndRequests.put(requestTime, timesAndRequests.get(requestTime) + 1);
-
-            if (timesAndRequests.get(requestTime) > 3) {
-                dropped.incrementAndGet();
-                System.out.printf("Dropped[%d]: %d\n", requestTime, (i + 1));
-                continue;
-            }
-
-            int reqLast10Seconds = timesAndRequests.keySet()
-                                                   .stream()
-                                                   .filter(time -> time >= Math.max(1, requestTime - 9) && time <= requestTime)
-                                                   .mapToInt(timesAndRequests::get)
-                                                   .sum();
-
-            if (reqLast10Seconds > 20) {
-                dropped.incrementAndGet();
-                System.out.printf("Dropped[%d]: %d\n", requestTime, (i + 1));
-                continue;
-            }
-
-            int reqLast60Seconds = timesAndRequests.keySet()
-                                                   .stream()
-                                                   .filter(time -> time >= Math.max(1, requestTime - 59) && time <= requestTime)
-                                                   .mapToInt(timesAndRequests::get)
-                                                   .sum();
-
-            if (reqLast60Seconds > 60) {
-                dropped.incrementAndGet();
-                System.out.printf("Dropped[%d]: %d\n", requestTime, (i + 1));
-                continue;
-            }
-
-            System.out.printf("Not Dropped[%d]: %d\n", requestTime, (i + 1));
+        for (int request : requestTimes) {
+            requestNumbers.putIfAbsent(request, 0);
+            requestNumbers.put(request, requestNumbers.get(request) + 1);
         }
 
-        return dropped.get();
+        int dropped = 0;
+        int last10Sec = 0;
+        int last60Sec = 0;
+
+        for (int instant : requestNumbers.keySet()) {
+            int currentRequests = requestNumbers.get(instant);
+
+            last10Sec += currentRequests;
+            last60Sec += currentRequests;
+
+            if (requestNumbers.containsKey(instant - 10)) {
+                last10Sec -= requestNumbers.get(instant - 10);
+            }
+
+            if (requestNumbers.containsKey(instant - 60)) {
+                last60Sec -= requestNumbers.get(instant - 60);
+            }
+
+            int drop3 = 0;
+            int drop10 = 0;
+            int drop60 = 0;
+
+            if (currentRequests > 3) {
+                drop3 = currentRequests - 3;
+            }
+
+            if (last10Sec > 20) {
+                drop10 = last10Sec - 20;
+            }
+
+            if (last60Sec > 60) {
+                drop60 = last60Sec - 60;
+            }
+
+            dropped += IntStream.of(drop3, drop10, drop60).max().getAsInt();
+        }
+
+        return dropped;
     }
 }
